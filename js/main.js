f3c072ccc5cee56937c058464ba5f5e7584fcc10
kwboyd <kwboyd@live.unc.edukwboyd$ git config --global user.email kwboyd@live.unc.edu>
2017-01-26 18:46:27 -0500

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// hint -- remove a particular element from an array: http://stackoverflow.com/questions/5767325/how-to-remove-a-particular-element-from-an-array-in-javascript
+// hint -- add an item to an array: http://stackoverflow.com/questions/351409/how-to-append-something-to-an-array
+// hint -- you may need an "editingItem" variable in your data object
+
+new Vue({
+  el: '#app',
+  data: {
+    tasks: [
+      {
+        toDoItemTitle: 'First task',
+        toDoItemContent: 'Example text'
+      }
+    ]
+    // toDoItemTitleInput: '',
+    // toDoItemContentInput: ''
+  },
+  methods: {
+    addTask: function() {
+      this.tasks.push ({
+        toDoItemTitle: this.toDoItemTitleInput,
+        toDoItemContent: this.toDoItemContentInput
+      })
+      // toDoItemContentInput: '',
+      // toDoItemTitleInput: ''
+    },
+    deleteTask: function(i) {
+      this.tasks.splice(i, 1)
+    }
+    // cancelTask: function() {
+    //   toDoItemTitleInput: '',
+    //   toDoItemContentInput: ''
+    // }
+  }
+});
