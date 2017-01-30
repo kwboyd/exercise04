777f582d9c984558c33953aa15b962122ee3eef9
kwboyd <kwboyd@live.unc.edukwboyd$ git config --global user.email kwboyd@live.unc.edu>
2017-01-30 12:43:03 -0500

    Added edit functionality.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 // hint -- remove a particular element from an array: http://stackoverflow.com/questions/5767325/how-to-remove-a-particular-element-from-an-array-in-javascript
 // hint -- add an item to an array: http://stackoverflow.com/questions/351409/how-to-append-something-to-an-array
 // hint -- you may need an "editingItem" variable in your data object
-
 new Vue({
   el: '#app',
   data: {
@@ -12,7 +11,10 @@ new Vue({
       }
     ],
     toDoItemTitleInput: '',
-    toDoItemContentInput: ''
+    toDoItemContentInput: '',
+    inputingItem: false,
+    editingTask: false,
+    taskIndex: null
   },
   methods: {
     addTask: function() {
@@ -22,13 +24,33 @@ new Vue({
       });
       this.toDoItemContentInput =  '';
       this.toDoItemTitleInput =  '';
+      this.inputingItem = false;
     },
     deleteTask: function(i) {
       this.tasks.splice(i, 1)
     },
     cancelTask: function() {
-    this.toDoItemTitleInput = '',
-    this.toDoItemContentInput = ''
+      this.toDoItemTitleInput = '',
+      this.toDoItemContentInput = '',
+      this.inputingItem = false;
+    },
+    inputTask: function() {
+      this.inputingItem = true;
+    },
+    editTask: function(i) {
+      this.editingTask = true;
+      this.toDoItemTitleInput = this.tasks[i].toDoItemTitle;
+      this.toDoItemContentInput = this.tasks[i].toDoItemContent;
+      this.inputingItem = true;
+      this.taskIndex = i;
+    },
+    finishEditing: function() {
+      this.tasks[this.taskIndex].toDoItemTitle = this.toDoItemTitleInput;
+      this.tasks[this.taskIndex].toDoItemContent = this.toDoItemContentInput;
+      this.editingTask = false;
+      this.inputingItem = false;
+      this.toDoItemTitleInput = '';
+      this.toDoItemContentInput = '';
     }
   }
 });
